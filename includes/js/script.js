37253852e4e90d8ba1092ea7f6c75cb4ce4b8556
rrodriguez112 <rrodriguez112@berkeley.edu>
2017-10-03 00:54:55 -0700

    NEW SITE BOI
    ay

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,31 +1,6 @@
-/*
-
-My Custom JS
-============
-
-Author:  Brad Hussey
-Updated: August 2013
-Notes:	 Hand coded for Udemy.com
-
-*/
-
-$(function() {
-	
-	$('#alertMe').click(function(e) {
-		
-		e.preventDefault();
-		
-		$('#successAlert').slideDown();
-		
-	});
-	
-	$('a.pop').click(function(e) {
-		e.preventDefault();
-	});
-	
-	$('a.pop').popover();
-	
-	$('[rel="tooltip"]').tooltip();
-	
-	
-});
+$(document).ready(function() {
+ // all your jQuery code goes in here!
+ $('#ticker-text').typeIt({
+    speed:50;
+ })
+}
--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
-	$('#scroll-me').click(function(){
-		$('body').animate({
-			scrollTop: $('#portfolio').offset().top
-		},'slow');
-		)
-	}
+ // all your jQuery code goes in here!
+	$('#ticker-text').typeIt({
+     speed: 50,
+     autoStart: false
+	})
+
+	.tiType('Wll')
+	.tiPause(500)
+	.tiDelete(2)
+	.tiType('ell, ')
+	.tiPause(1000)
+	.tiType('I guess I\'m typing..')
+	.tiBreak() .tiPause(750)
+	.tiType(' but I don\'t really know what to say')
+	.tiSettings({speed: 700})
+	.tiType('...')
+	.tiPause(750)
+	.tiSettings({speed: 50})
+	.tiDelete()
+	.tiType('IS THAT SO <strong>WRONG??</strong>');
 })
